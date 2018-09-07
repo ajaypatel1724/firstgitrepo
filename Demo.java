1044e2b99f042bb06aedaf2ed5305e873fda86d8
ajaypatel1724 <ajaypatel1724@gmail.com>
2018-09-07 16:25:31 +0530

    add operation

--- Demo.java
+++ Demo.java
@@ -1,5 +1,8 @@
 public Demo{
   public static void main(String[] args){
     System.out.println("Demo Git, First Program");
+    int i=1;
+    int j=1;
+    System.out.println("Additional data is : " + i+j);
   }
 }
