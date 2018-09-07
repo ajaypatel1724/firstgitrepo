3f001fbbf9a8609f0ca88dc17f3d24164a93f880
ajaypatel1724 <ajaypatel1724@gmail.com>
2018-09-07 16:32:03 +0530

    firstbranch update message

--- Demo.java
+++ Demo.java
@@ -1,8 +1,9 @@
 public Demo{
   public static void main(String[] args){
     System.out.println("Demo Git, First Program");
-    int i=1;
-    int j=1;
-    System.out.println("Additional data is : " + i+j);
+    int i=10;
+    int j=10;
+    System.out.println("Addition data is : " + i+j);
+    
   }
 }
